f5a4ec41819db058919d14d07f986405fbe10571
ESTELA ALVES DE MORAES <23111781@CFP127ALUNO.LOCAL>
2024-02-06 16:41:31 -0300

    mudança

--- pagina-inicial/inicio.js
+++ pagina-inicial/inicio.js
@@ -1,42 +1,83 @@
 'use strict'
 
-const botaoEditar = document.getElementById('editar')
+let usuarioId = 3
 
-const modoEditar = () =>{
+async function getTarefas() {
+    const responseApi = await fetch('http://localhost:5080/tarefas')
+    const listaTarefas = responseApi.json();
+
+    return listaTarefas
+}
+async function getUsuarios() {
+    const responseApi = await fetch('http://localhost:5080/usuario')
+    const listaUsuarios = responseApi.json();
+
+    return listaUsuarios
+}
+
+
+const criarTarefa = (tarefa) =>{
+
+    const containerDeTarefas = document.getElementById('tarefas')
+
+    const containerTarefa = document.createElement('div')
+    containerTarefa.classList.add('tarefa')
+
+    const botaoEditarSalvar = document.createElement('button')
+    botaoEditarSalvar.classList.add('icone-e-cor')
+    const iconeEditar = document.createElement('img')
+    iconeEditar.src = '../img/edit.svg'
+
+    const infoTarefa = document.createElement('div')
+    infoTarefa.classList.add('info-tarefa')
+
+    const tituloTarefa = document.createElement('span')
+    tituloTarefa.textContent = tarefa.descrição
+    tituloTarefa.classList.add('titulo-tarefa')
+    
+    const dataConclusao = document.createElement('span')
+    dataConclusao.textContent = tarefa.dataConclusão
+    dataConclusao.classList.add('data-tarefa')
+
+    botaoEditarSalvar.appendChild(iconeEditar)
+    infoTarefa.replaceChildren(tituloTarefa, dataConclusao)
+    containerTarefa.replaceChildren(botaoEditarSalvar, infoTarefa)
+    containerDeTarefas.appendChild(containerTarefa)
+
+    botaoEditarSalvar.addEventListener('click', modoEditar)
+ }
+
+ const carregarTarefas = async () =>{
+     const tarefas = await getTarefas()
+     tarefas.forEach(tarefa =>{
+         if(tarefa.idUsuario == usuarioId)
+          criarTarefa(tarefa)
+     })
+ }
+
+ const carregarUsuario = async () =>{
+     const usuarios = await getUsuarios()
+     usuarios.forEach(usuario =>{
+        if(usuario.id == usuarioId){
+            let nomeUsuario = usuario.nome.split(" ")
+            
+            let campoNomeUsuario = document.getElementById('nomeUsuario')
+            campoNomeUsuario.textContent = nomeUsuario[0]
+        }
+     })
+ }
+
+
+const modoEditar = (botaoEditar) =>{
+
+    console.log('clicou')
 
     botaoEditar.classList.toggle('edit-mode')
 
     if(botaoEditar.classList == 'edit-mode'){
         botaoEditar.innerHTML = '<img src="../img/save.svg" alt="">'
     }
-
-    const infoTarefa = document.getElementById('info-tarefa')
-    infoTarefa.classList.toggle('edit-mode')
-    const titulo = document.querySelector('.titulo-tarefa')
-    const dataConclusao = document.querySelector('.data-tarefa')
-
-    if(infoTarefa.classList == 'edit-mode'){
-        const inputTitulo = document.createElement('span')
-        inputTitulo.value = titulo.innerHTML
-    
-        const inputData = document.createElement('span')
-        inputData.value = dataConclusao.innerHTML
-    
-        infoTarefa.replaceChildren(inputTitulo, inputData)
-    }else{
-        const inputTitulo = document.createElement('input')
-        inputTitulo.value = titulo.innerHTML
-        infoTarefa.innerHTML = "<input>"
-    
-        const inputData = document.createElement('input')
-        inputData.value = dataConclusao.innerHTML
-        infoTarefa.innerHTML = "<input>"
-    
-        infoTarefa.replaceChildren(inputTitulo, inputData)
-    }
-
-
-
 }
 
-botaoEditar.addEventListener('click', modoEditar)
+carregarUsuario()
+carregarTarefas()
