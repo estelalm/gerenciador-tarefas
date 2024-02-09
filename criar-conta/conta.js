ac06b4b87f746813567bb4a0b23c58e0709b27cf
Estela Alves <123739850+estelalm@users.noreply.github.com>
2024-02-09 16:09:35 -0300

    criar conta e correções

---
+++ criar-conta/conta.js
@@ -0,0 +1,50 @@
+'use strict' 
+
+async function getUsuarios() {
+    const responseApi = await fetch('http://localhost:5080/usuario')
+    const listaUsuario = responseApi.json();
+
+    return listaUsuario
+}
+
+const botaoEntrar= document.getElementById('criar')
+
+const criarConta = () =>{
+
+
+    let nomeUsuario = document.getElementById('nome').value
+    let emailUsuario =  document.getElementById('email').value
+    let senhaUsuario = document.getElementById('senha').value
+    let senhaConfirmada = document.getElementById('confirma-senha').value
+
+    if(nomeUsuario == "" || emailUsuario == "" || senhaUsuario == ""|| senhaConfirmada == ""){
+        alert('Preencha todos os campos!')
+    }else if(!(senhaUsuario == senhaConfirmada)){
+        alert('As senhas devem ser iguais')
+    } else {
+        alert('Conta criada, voltando para a página de Login.')
+
+
+       let usuarioJSON = {}
+        getUsuarios().then(usuariosArray =>{
+        usuarioJSON.id = usuariosArray.length ++
+    })
+        usuarioJSON = {
+            "nome": nomeUsuario,
+            "email": emailUsuario,
+            "senha": senhaUsuario
+        }
+
+         fetch('http://localhost:5080/usuario' , {
+            method: 'POST', 
+            headers:{
+            'content-type': 'application/json',
+            }, 
+            body: JSON.stringify(usuarioJSON)
+            })
+            
+        window.location.assign('../index.html')
+    }
+}
+
+botaoEntrar.addEventListener('click', criarConta)
